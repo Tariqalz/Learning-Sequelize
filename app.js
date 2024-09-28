2b05d92223a6ad02b0d15097e41957cb1ad97a13
Tariq Alz <tariqalz13579@outlook.com>
2024-09-28 22:15:16 +0300

    add uuid, hide id and the use of migrations

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.post("/users", async (req, res) => {
 
 app.listen({ port: 5000 }, async () => {
   console.log("Server up on https://localhost:5000");
-  await sequelize.sync({ force: true });
-  console.log("Database synced!");
+  await sequelize.authenticate();
+  console.log("Database Connected!");
 });
