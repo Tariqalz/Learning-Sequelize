883c3cb50413f0322a4b6559cf860e803b0260bf
Tariq Alz <tariqalz13579@outlook.com>
2024-09-29 14:50:16 +0300

    implement validation, add delete and update

--- models/user.js
+++ models/user.js
@@ -24,14 +24,27 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a name" },
+          notEmpty: { msg: "Name must not be empty" },
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a email" },
+          notEmpty: { msg: "Email must not be empty" },
+          isEmail: { msg: "Must be a valid email address" },
+        },
       },
       role: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a role" },
+          notEmpty: { msg: "Role must not be empty" },
+        },
       },
     },
     {
